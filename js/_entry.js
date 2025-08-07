73552fe69397a4442dcae44d677246adb55aff77
Mehluli Hikwa <mehlulihikwa@gmail.com>
2025-08-07 03:15:19 +0200

    Update Script Imports

---
+++ js/_entry.js
@@ -0,0 +1,10 @@
+// Import Popper from node_modules
+// import { createPopper } from "@popperjs/core";
+// Or, if you want both:
+// import "./popper.js";
+
+
+import "@popperjs/core";
+import "./tippy.js"; // to run this we need popper.js
+import "./game.js";
+import "./theme.js";
